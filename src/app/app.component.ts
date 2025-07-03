8b22589e89fe897db40444b7be56f5d648cd03ff
Raji G <rajith0417@gmail.com>
2025-07-04 00:23:13 +0530

    fix: native element error fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,31 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import videojs from 'video.js'
-import type Player from 'video.js/dist/types/player';
+// import videojs from 'video.js'
+// import type Player from 'video.js/dist/types/player';
+import { HeaderComponent } from "./components/header/header.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { VideoplayerComponent } from "./components/videoplayer/videoplayer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, VideoplayerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'skillGround';
-  @ViewChild('target', { static: true }) target!: ElementRef;
+  // @ViewChild('target', { static: false }) target!: ElementRef;
 
-  player!: Player;
+  // player!: Player;
 
-  ngAfterViewInit(): void {
-    this.player = videojs(this.target.nativeElement);
-  }
+  // ngAfterViewInit(): void {
+  //   this.player = videojs(this.target.nativeElement);
+  // }
 
-  ngOnDestroy(): void {
-    if (this.player) {
-      this.player.dispose();
-    }
-  }
+  // ngOnDestroy(): void {
+  //   if (this.player) {
+  //     this.player.dispose();
+  //   }
+  // }
 }
--- src/app/components/videoplayer/videoplayer.component.ts
+++ src/app/components/videoplayer/videoplayer.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { VideoService } from '../../services/video.service';
 import { Video } from '../models/video.model';
+import videojs from 'video.js'
+import type Player from 'video.js/dist/types/player';
 
 @Component({
   selector: 'app-videoplayer',
@@ -12,6 +14,8 @@ import { Video } from '../models/video.model';
 export class VideoplayerComponent implements OnInit{
 
   videos:Video[] = [];
+  player!: Player;
+  @ViewChild('target', { static: false }) target!: ElementRef;
 
   constructor(private videoService: VideoService){}
 
@@ -20,5 +24,15 @@ export class VideoplayerComponent implements OnInit{
       this.videos = response;
       console.log(response);
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.player = videojs(this.target.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    if (this.player) {
+      this.player.dispose();
+    }
   }
 }
