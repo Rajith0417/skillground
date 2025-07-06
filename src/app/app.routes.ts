fd0e71c4ea2418cbe4b763287f68cd0b572c96c9
Raji G <rajith0417@gmail.com>
2025-07-06 22:04:07 +0530

    feat: routers added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { VideoplayerComponent } from './components/videoplayer/videoplayer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'video', pathMatch: 'full' },
+  { path: 'video', component: VideoplayerComponent },
+  { path: 'video/:id', component: VideoplayerComponent }
+];
