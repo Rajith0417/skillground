15a5e3e8e57f672520e2f9033b759b873d001ca5
Raji G <rajith0417@gmail.com>
2025-07-06 22:05:03 +0530

    material modules added

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { VideoService } from '../../services/video.service';
+import { Video } from '../models/video.model';
+import { CommonModule } from '@angular/common';
+import { VideoResponse } from '../models/videoResponse.model';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [MatListModule, MatPaginatorModule, MatIconModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+
+  videos:Video[] = [];
+
+  constructor(private videoService: VideoService){
+
+  }
+
+  ngOnInit(): void {
+    this.videoService.getVideos().subscribe(response => {
+      // this.videos = response.categories.videos
+      // console.log(response.categories[0].videos);
+      this.videos = response.categories?.[0]?.videos ?? [];
+      console.log(this.videos);
+    });
+  }
 
 }
